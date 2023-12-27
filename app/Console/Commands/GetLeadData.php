afc973b1e25036a14968361bdcca4aeaa181a277
Faiz miya <faizmiya110@gmail.com>
2023-12-27 10:09:39 +0530

    first commit

--- app/Console/Commands/GetLeadData.php
+++ app/Console/Commands/GetLeadData.php
@@ -52,31 +52,63 @@ class GetLeadData extends Command
         $data = $response->json();
         logger($data);
         foreach ($data['RESPONSE'] as $leadData) {
-            ApiLead::create([
-                'unique_query_id' => $leadData['UNIQUE_QUERY_ID'] ?? null,
-                'query_type' => $leadData['QUERY_TYPE'] ?? null,
-                'query_time' => $leadData['QUERY_TIME'] ?? null,
-                'sender_name' => $leadData['SENDER_NAME'] ?? null,
-                'sender_mobile' => $leadData['SENDER_MOBILE'] ?? null,
-                'sender_email' => $leadData['SENDER_EMAIL'] ?? null,
-                'subject' => $leadData['SUBJECT'] ?? null,
-                'sender_company' => $leadData['SENDER_COMPANY'] ?? null,
-                'sender_address' => $leadData['SENDER_ADDRESS'] ?? null,
-                'sender_city' => $leadData['SENDER_CITY'] ?? null,
-                'sender_state' => $leadData['SENDER_STATE'] ?? null,
-                'sender_pincode' => $leadData['SENDER_PINCODE'] ?? null,
-                'sender_country_iso' => $leadData['SENDER_COUNTRY_ISO'] ?? null,
-                'sender_mobile_alt' => $leadData['SENDER_MOBILE_ALT'] ?? null,
-                'sender_phone' => $leadData['SENDER_PHONE'] ?? null,
-                'sender_phone_alt' => $leadData['SENDER_PHONE_ALT'] ?? null,
-                'sender_email_alt' => $leadData['SENDER_EMAIL_ALT'] ?? null,
-                'query_product_name' => $leadData['QUERY_PRODUCT_NAME'] ?? null,
-                'query_message' => $leadData['QUERY_MESSAGE'] ?? null,
-                'query_mcat_name' => $leadData['QUERY_MCAT_NAME'] ?? null,
-                'call_duration' => $leadData['CALL_DURATION'] ?? null,
-                'receiver_mobile' => $leadData['RECEIVER_MOBILE'] ?? null,
-            ]);
+            // Check if the record with the UNIQUE_QUERY_ID already exists
+            $existingLead = ApiLead::where('unique_query_id', $leadData['UNIQUE_QUERY_ID'])->first();
+
+            if (!$existingLead) {
+                // If not, create a new record
+                ApiLead::create([
+                    'unique_query_id' => $leadData['UNIQUE_QUERY_ID'] ?? null,
+                    'query_type' => $leadData['QUERY_TYPE'] ?? null,
+                    'query_time' => $leadData['QUERY_TIME'] ?? null,
+                    'sender_name' => $leadData['SENDER_NAME'] ?? null,
+                    'sender_mobile' => $leadData['SENDER_MOBILE'] ?? null,
+                    'sender_email' => $leadData['SENDER_EMAIL'] ?? null,
+                    'subject' => $leadData['SUBJECT'] ?? null,
+                    'sender_company' => $leadData['SENDER_COMPANY'] ?? null,
+                    'sender_address' => $leadData['SENDER_ADDRESS'] ?? null,
+                    'sender_city' => $leadData['SENDER_CITY'] ?? null,
+                    'sender_state' => $leadData['SENDER_STATE'] ?? null,
+                    'sender_pincode' => $leadData['SENDER_PINCODE'] ?? null,
+                    'sender_country_iso' => $leadData['SENDER_COUNTRY_ISO'] ?? null,
+                    'sender_mobile_alt' => $leadData['SENDER_MOBILE_ALT'] ?? null,
+                    'sender_phone' => $leadData['SENDER_PHONE'] ?? null,
+                    'sender_phone_alt' => $leadData['SENDER_PHONE_ALT'] ?? null,
+                    'sender_email_alt' => $leadData['SENDER_EMAIL_ALT'] ?? null,
+                    'query_product_name' => $leadData['QUERY_PRODUCT_NAME'] ?? null,
+                    'query_message' => $leadData['QUERY_MESSAGE'] ?? null,
+                    'query_mcat_name' => $leadData['QUERY_MCAT_NAME'] ?? null,
+                    'call_duration' => $leadData['CALL_DURATION'] ?? null,
+                    'receiver_mobile' => $leadData['RECEIVER_MOBILE'] ?? null,
+                ]);
+            }
         }
+        // foreach ($data['RESPONSE'] as $leadData) {
+        //     ApiLead::create([
+        //         'unique_query_id' => $leadData['UNIQUE_QUERY_ID'] ?? null,
+        //         'query_type' => $leadData['QUERY_TYPE'] ?? null,
+        //         'query_time' => $leadData['QUERY_TIME'] ?? null,
+        //         'sender_name' => $leadData['SENDER_NAME'] ?? null,
+        //         'sender_mobile' => $leadData['SENDER_MOBILE'] ?? null,
+        //         'sender_email' => $leadData['SENDER_EMAIL'] ?? null,
+        //         'subject' => $leadData['SUBJECT'] ?? null,
+        //         'sender_company' => $leadData['SENDER_COMPANY'] ?? null,
+        //         'sender_address' => $leadData['SENDER_ADDRESS'] ?? null,
+        //         'sender_city' => $leadData['SENDER_CITY'] ?? null,
+        //         'sender_state' => $leadData['SENDER_STATE'] ?? null,
+        //         'sender_pincode' => $leadData['SENDER_PINCODE'] ?? null,
+        //         'sender_country_iso' => $leadData['SENDER_COUNTRY_ISO'] ?? null,
+        //         'sender_mobile_alt' => $leadData['SENDER_MOBILE_ALT'] ?? null,
+        //         'sender_phone' => $leadData['SENDER_PHONE'] ?? null,
+        //         'sender_phone_alt' => $leadData['SENDER_PHONE_ALT'] ?? null,
+        //         'sender_email_alt' => $leadData['SENDER_EMAIL_ALT'] ?? null,
+        //         'query_product_name' => $leadData['QUERY_PRODUCT_NAME'] ?? null,
+        //         'query_message' => $leadData['QUERY_MESSAGE'] ?? null,
+        //         'query_mcat_name' => $leadData['QUERY_MCAT_NAME'] ?? null,
+        //         'call_duration' => $leadData['CALL_DURATION'] ?? null,
+        //         'receiver_mobile' => $leadData['RECEIVER_MOBILE'] ?? null,
+        //     ]);
+        // }
         $this->info('Lead data fetched successfully!');
     }
 }
